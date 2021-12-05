25d4906ab9d9ba1ba1d5b385871846a69d235292
IvanVnucec <vnucec.ivan@gmail.com>
2021-12-05 17:06:58 +0100

    add Array class

--- heapless_cpp.hpp
+++ heapless_cpp.hpp
@@ -1,9 +1,32 @@
 #ifndef HEAPLESS_CPP_HPP
 #define HEAPLESS_CPP_HPP
 
-namespace hcpp
+namespace hlcpp
 {
-    int hi;
+
+template <typename T, unsigned L>
+class Array
+{
+private:
+    T m_buffer[L];
+    unsigned m_len = L;
+
+public:
+    Array()
+    {
+    }
+
+    ~Array()
+    {
+    }
+
+    unsigned len()
+    {
+        return m_len;
+    }
+
+};
+
 }
 
 #endif // #ifndef HEAPLESS_CPP_HPP
--- tests/run_all.cpp
+++ tests/run_all.cpp
@@ -9,18 +9,20 @@ void test_teardown(void)
 {
 }
 
-MU_TEST(test_example){
-	mu_assert(true, "");
+MU_TEST(test_array_len){
+    hlcpp::Array<int, 10> a;
+
+    mu_check(a.len() == 10);
 }
 
-MU_TEST_SUITE(test_suite) {
+MU_TEST_SUITE(test_array) {
 	MU_SUITE_CONFIGURE(&test_setup, &test_teardown);
 
-	MU_RUN_TEST(test_example);
+	MU_RUN_TEST(test_array_len);
 }
 
 int main(int argc, char *argv[]) {
-	MU_RUN_SUITE(test_suite);
+	MU_RUN_SUITE(test_array);
 	MU_REPORT();
 	return MU_EXIT_CODE;
 }
